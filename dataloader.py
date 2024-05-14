d8910f802d38864133cecab5703b1ab3365d27a2
yanisadel <yanis_adel@hotmail.com>
2024-05-14 10:53:32 +0200

    refactor: Change name of the function that loads MNIST data

--- dataloader.py
+++ dataloader.py
@@ -1,7 +1,7 @@
 import tensorflow_datasets as tfds
 
 
-def load_data():
+def load_unprocessed_mnist_data():
     data, info = tfds.load(
         name="mnist", data_dir="/tmp/tfds", as_supervised=True, with_info=True
     )
