add25c916b219f8ec7804e7b6c91b07b6bc7eed6
yanisadel <yanis_adel@hotmail.com>
2024-05-14 10:50:58 +0200

    feat: Load unprocessed MNIST data with tensorflow_datasets

---
+++ dataloader.py
@@ -0,0 +1,12 @@
+import tensorflow_datasets as tfds
+
+
+def load_data():
+    data, info = tfds.load(
+        name="mnist", data_dir="/tmp/tfds", as_supervised=True, with_info=True
+    )
+
+    data_train = data["train"]
+    data_test = data["test"]
+
+    return data_train, data_test, info
