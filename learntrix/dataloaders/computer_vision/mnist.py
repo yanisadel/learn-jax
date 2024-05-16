65ed534ecd101b14f1f206fe179df9880b8a6128
yanisadel <yanis_adel@hotmail.com>
2024-05-16 18:17:04 +0200

    refactor: Add dataloader for mnist in learantrix.dataloaders.computer_vision.mnist.py

---
+++ learntrix/dataloaders/computer_vision/mnist.py
@@ -0,0 +1,22 @@
+from collections.abc import Iterator
+
+import tensorflow_datasets as tfds
+
+from learntrix.types import Batch
+
+
+def load_mnist_dataset(
+    split: str,
+    *,
+    shuffle: bool,
+    batch_size: int,
+) -> Iterator[Batch]:
+    """Loads the MNIST dataset."""
+    ds, ds_info = tfds.load("mnist:3.*.*", split=split, with_info=True)
+    ds.cache()
+    if shuffle:
+        ds = ds.shuffle(ds_info.splits[split].num_examples, seed=0)
+    ds = ds.repeat()
+    ds = ds.batch(batch_size)
+    ds = ds.map(lambda x: Batch(**x))
+    return iter(tfds.as_numpy(ds))
